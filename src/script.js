18b4f266bb74f0b70b8f0840c9ba8e773915627f
imsan <ibrahimattiasan@gmail.com>
2022-07-13 10:35:00 -0800

    Fixed Compliation Bug

--- src/script.js
+++ src/script.js
@@ -4,32 +4,38 @@
     1. When submit button is clicked the display for the thank you page becomes flex, and z-index becomes higher than 2
         1a. Note you cannot add transitions to display property in css. 
         
-    2. When clicking on the ratings button, the value of that button should be displayed instead of the generic text
+    2. When clicking on the ratings button, the value of that button should be displayed instead of the generic text 
+    
 
 */
 
-const submitBtn = document.getElementById("submit_btn"); // 1
-const thankyouCard = document.getElementById("thankyou_card") //1
+const submitBtn = document.getElementById("submit_btn"); 
+const thankyouCard = document.getElementById("thankyou_card") 
 
-const oneStar = document.getElementById('1'); //2
-const twoStar = document.getElementById('2'); //2
-const threeStar = document.getElementById('3'); //2
-const fourStar = document.getElementById('4'); //2
-const fiveStar = document.getElementById('5'); //2
-const displayRate = document.getElementById("rating"); //2
+const oneStar = document.getElementById('1'); 
+const twoStar = document.getElementById('2'); 
+const threeStar = document.getElementById('3'); 
+const fourStar = document.getElementById('4'); 
+const fiveStar = document.getElementById('5'); 
+const displayRate = document.getElementById("rating");
 
-const submitClick = () => {  //1
+const rateText = document.getElementById("rate__text"); 
+const rateTitle = document.getElementById("rate__title"); 
+const rateIcon = document.getElementById("rate__star--icon"); 
+
+const submitClick = () => {  
     thankyouCard.style.display = "flex"; 
     thankyouCard.style.zIndex = "10";
 }
 
 
 
+submitBtn.addEventListener("click", submitClick); 
 
-submitBtn.addEventListener("click", submitClick); // 1 
 
 oneStar.addEventListener("click", () => { 
     displayRate.innerText = oneStar.dataset.value;
+    
 })
 
 twoStar.addEventListener("click", () => { 
@@ -38,15 +44,30 @@ twoStar.addEventListener("click", () => {
 
 threeStar.addEventListener("click", () => { 
     displayRate.innerText = threeStar.dataset.value;
+    
 })
 
 fourStar.addEventListener("click", () => { 
     displayRate.innerText = fourStar.dataset.value;
+    
 })
 
 fiveStar.addEventListener("click", () => { 
     displayRate.innerText = fiveStar.dataset.value;
+    
+})
+
+rateIcon.addEventListener("click", ()=> { 
+     displayRate.innerText = " ";
 })
 
 
+rateText.addEventListener("click", ()=> { 
+     displayRate.innerText = " ";
+})
+
+
+rateTitle.addEventListener("click", ()=> { 
+     displayRate.innerText = " ";
+})
 
